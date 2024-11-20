db4f3cc1ed0fed6ebde2784c1db364f80eb0ac55
IsmaCar <carballomartinismael@gmail.com>
2024-11-20 23:02:18 +0100

    Create exercises reservas

---
+++ 01-dom-reservas/src/components/divCard/divCard.js
@@ -0,0 +1,13 @@
+// const divCard = document.createElement("div");
+//     divCard.id = "card";
+//     divCard.classList.add("principal card");
+
+//     //Crear parrafo
+//     const p = document.createElement("p");
+//     p.textContent = "Hola soy un párrafo"
+//     divCard.appendChild(p);
+
+//     //Crear botón
+//     const mensajeBtn = document.createElement("button");
+//     mensajeBtn.textContent = "Mostrar mensaje";
+//     divCard.appendChild(mensajeBtn);
---
+++ 01-dom-reservas/src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+//Spinner
+
+const createSpinner = () => {
+    const spinner = document.createElement('div');
+    spinner.id = "spinner";
+    spinner.classList.add('hidden spinner');
+    spinner.style.color = "red";
+    spinner.textContent = "Cargando...";
+
+}
+
+const showSpinner = () => {
+
+}
+
+const hideSpinner = () => {
+
+}
